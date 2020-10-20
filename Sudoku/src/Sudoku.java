80cda7575a47b2e6c1d7a06ebc636afd915d97f7
Ekinsxd <55798068+Ekinsxd@users.noreply.github.com>
2020-10-19 20:44:34 -0400

    initial release

---
+++ Sudoku/src/Sudoku.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+
+public class Sudoku {
+
+    public static void main (String[] args)
+    {
+        JFrame frame = new JFrame ("Sudoku");
+        frame.setDefaultCloseOperation (JFrame.EXIT_ON_CLOSE);
+
+        SudokuPanel panel = new SudokuPanel();
+
+        frame.getContentPane().add(panel);
+
+        frame.setSize(600,650);
+        frame.setVisible(true);
+    }
+
+
+
+}
+
