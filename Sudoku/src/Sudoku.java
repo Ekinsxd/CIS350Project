72386d257ff902fb01006e83c723965a2254eee4
davism2 <64663157+davism2@users.noreply.github.com>
2020-11-04 18:42:39 -0500

    Update Sudoku.java
    Fixed spelling issue in Class documentation

--- Sudoku/src/Sudoku.java
+++ Sudoku/src/Sudoku.java
@@ -2,12 +2,14 @@ import javax.swing.*;
 
 /**
  * A class that initizlies and constructs the Soduku GUI
- * @author Matthew DAvis, Ethan Tran, and Cole Hyink
+ *
+ * @author Matthew Davis, Ethan Tran, and Cole Hyink
  */
 public class Sudoku {
 
     /**
      *  Initilizes and constructs Soduku GUI
+     *
      * @param args
      */
     public static void main (String[] args)
